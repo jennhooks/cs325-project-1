3dbfcc699d5da740d95eefdefb5a7e3c2301a394
Jennifer Hooks <johooks@siue.edu>
2024-09-18 22:26:27 -0500

    Implement function to send prompts to model

--- main.py
+++ main.py
@@ -6,22 +6,35 @@
 import requests
 
 def main():
+    try:
+        response = send_prompt('Why is the sky blue?')
+        print(response)
+
+        response = send_prompt('Why is Mars red?')
+        print(response)
+
+        response = send_prompt('What is 6 + 4?')
+        print(response)
+    except requests.exceptions.RequestException:
+        print('Failed to send prompt')
+
+def send_prompt(prompt):
     # Url to the Ollama server hosted on localhost.
     url = 'http://localhost:11434/api/generate'
     # Data to be sent to the server.
     json_data = {
             'model': 'phi3:mini', 
-            'prompt': 'Why is the sky blue?',
+            'prompt': prompt,
             'stream': False, # Return only complete responses.
             }
-    # Send POST request to server.
-    response = requests.post(url, json = json_data)
-    # If successful, print the completion.
-    if response.status_code == 200:
-        print(response.json()['response'])
-    # Otherwise, print error.
-    else:
+    # Send POST request to server and try to get the response.
+    try:
+        response = requests.post(url, json = json_data)
+        return response.json()['response']
+    # If it failed
+    except requests.exceptions.RequestException:
         print('POST request failed')
+        raise
 
 if __name__ == '__main__':
     main()
