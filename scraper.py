5d11796d52be8b0e498bef890b4f8c7b93d7cffd
Jennifer Hooks <johooks@siue.edu>
2024-10-23 21:40:22 -0500

    Implement getting all reviews from multiple products

--- scraper.py
+++ scraper.py
@@ -6,17 +6,25 @@ import requests
 
 def main():
     product_url_list = get_product_links()
-    # Product 1
-    response = requests.get(product_url_list[0])
-    soup = BeautifulSoup(response.text, 'html.parser')
-    all_reviews = get_all_reviews(soup)
-    for review in all_reviews:
-        print(review)
+    for url in product_url_list:
+        print(url)
+        all_reviews = get_all_reviews(url)
+        for review in all_reviews:
+            print(review)
+        print()
+        print()
 
 # Returns a list containing all reviews for a product.
-def get_all_reviews(soup):
+def get_all_reviews(url):
     review_list = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
     urls = get_review_links(soup)
+    # If the product only has one page of reviews this list will be empty
+    # because products with only one page of reviews don't have anything
+    # to extract urls from.
+    if not urls:
+        urls = [url]
     for url in urls:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -27,7 +35,7 @@ def get_all_reviews(soup):
 def get_reviews(soup):
     review_list = []
     for content in soup.find_all('p', itemprop="reviewBody"):
-        review_list.append(content.text)
+        review_list.append(content.text.rstrip('Read full review...'))
     return review_list
 
 # When given the html document of page 1 of the reviews it extracts the other
