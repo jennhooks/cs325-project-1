146b155e8bdf79d4234a1bf370e5e6f45a6ed6be
Jennifer Hooks <johooks@siue.edu>
2024-10-23 20:12:21 -0500

    Implement extracting page urls

--- scraper.py
+++ scraper.py
@@ -7,7 +7,19 @@ import requests
 def main():
     response = requests.get('https://www.ebay.com/urw/Available-2010-Titles-Enhanced-Web-Assign-Ser-Calculus-Early-Transcendentals-by-James-Stewart-2007-Hardcover-/product-reviews/53564728?_itm=226310510813')
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup.prettify())
+    url_list = get_comment_links(soup)
+    for url in url_list:
+        print(url)
+
+# When given the html document of page 1 of the reviews it extracts the other
+# page's urls.
+def get_comment_links(soup):
+    url_list = []
+    for link in soup.find_all('a'):
+        if not link.get('href') is None and '&pgn=' in link.get('href'):
+            if not link.get('href') in url_list:
+                url_list.append(link.get('href'))
+    return url_list
 
 if __name__ == '__main__':
     main()
