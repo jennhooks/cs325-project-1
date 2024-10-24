dae72763e36caba5dfba0c31f773f19e3327a614
Jennifer Hooks <johooks@siue.edu>
2024-10-23 19:16:00 -0500

    Add BeautifulSoup scraping

--- scraper.py
+++ scraper.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 # Scraper.py
 
+from bs4 import BeautifulSoup
+import requests
+
 def main():
-    pass
+    response = requests.get('https://www.ebay.com/urw/Available-2010-Titles-Enhanced-Web-Assign-Ser-Calculus-Early-Transcendentals-by-James-Stewart-2007-Hardcover-/product-reviews/53564728?_itm=226310510813')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.prettify())
 
 if __name__ == '__main__':
     main()
