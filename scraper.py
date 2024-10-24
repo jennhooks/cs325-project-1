5267f0dcdba6e31d6d879713534e7b6f237f9e89
Jennifer Hooks <johooks@siue.edu>
2024-10-23 20:55:52 -0500

    Implement extracting all reviews for a product

--- scraper.py
+++ scraper.py
@@ -9,9 +9,19 @@ def main():
     # Product 1
     response = requests.get(product_url_list[0])
     soup = BeautifulSoup(response.text, 'html.parser')
-    reviews = get_reviews(soup)
-    for review in reviews:
+    all_reviews = get_all_reviews(soup)
+    for review in all_reviews:
         print(review)
+
+# Returns a list containing all reviews for a product.
+def get_all_reviews(soup):
+    review_list = []
+    urls = get_review_links(soup)
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        review_list.extend(get_reviews(soup))
+    return review_list
 
 # Returns a list of strings where each entry in the list is one review from the page.
 def get_reviews(soup):
@@ -20,6 +30,16 @@ def get_reviews(soup):
         review_list.append(content.text)
     return review_list
 
+# When given the html document of page 1 of the reviews it extracts the other
+# page's urls.
+def get_review_links(soup):
+    url_list = []
+    for link in soup.find_all('a'):
+        if not link.get('href') is None and '&pgn=' in link.get('href'):
+            if not link.get('href') in url_list:
+                url_list.append(link.get('href'))
+    return url_list
+
 # Returns the list of product urls in the input file.
 def get_product_links():
     url_list = []
@@ -27,16 +47,6 @@ def get_product_links():
     with open(input_filename, 'r') as file:
         for line in file:
             url_list.append(line.rstrip())
-    return url_list
-
-# When given the html document of page 1 of the reviews it extracts the other
-# page's urls.
-def get_comment_links(soup):
-    url_list = []
-    for link in soup.find_all('a'):
-        if not link.get('href') is None and '&pgn=' in link.get('href'):
-            if not link.get('href') in url_list:
-                url_list.append(link.get('href'))
     return url_list
 
 if __name__ == '__main__':
