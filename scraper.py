d37b7cee793cf0b6231ef1e471109a8260ae7e3c
Jennifer Hooks <johooks@siue.edu>
2024-10-23 22:07:42 -0500

    Implement saving to output files

--- scraper.py
+++ scraper.py
@@ -5,14 +5,26 @@ from bs4 import BeautifulSoup
 import requests
 
 def main():
+    # Get urls from input file.
     product_url_list = get_product_links()
-    for url in product_url_list:
-        print(url)
-        all_reviews = get_all_reviews(url)
-        for review in all_reviews:
-            print(review)
-        print()
-        print()
+    # Define names for the output files.
+    product_versions = ['5th edition', '6th edition', '7th edition', '8th edition']
+    # Associate the urls with the output file names.
+    product_dict = {}
+    for i in range(0, len(product_url_list)):
+        product_dict[product_url_list[i]] = product_versions[i]
+
+    for product in product_dict:
+        all_reviews = get_all_reviews(product)
+        save_reviews(all_reviews, product_dict[product])
+
+# Saves the list of review strings to a file named 'PREFIX_reviews.txt'
+def save_reviews(reviews, file_prefix):
+    output_filename = file_prefix + ' reviews'
+    with open(output_filename, 'w') as file:
+        for review in reviews:
+            file.write(review)
+            file.write('\n')
 
 # Returns a list containing all reviews for a product.
 def get_all_reviews(url):
@@ -35,7 +47,7 @@ def get_all_reviews(url):
 def get_reviews(soup):
     review_list = []
     for content in soup.find_all('p', itemprop="reviewBody"):
-        review_list.append(content.text.rstrip('Read full review...'))
+        review_list.append(content.text)
     return review_list
 
 # When given the html document of page 1 of the reviews it extracts the other
